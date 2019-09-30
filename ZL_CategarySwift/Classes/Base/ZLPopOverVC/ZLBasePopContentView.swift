c9724879b39a04d40b623b12ef3ca15d85fe4808
Whity <liulei10luojia@163.com>
2019-09-30 14:21:34 +0800

    修改podspec

--- ZL_CategarySwift/Classes/Base/ZLPopOverVC/ZLBasePopContentView.swift
+++ ZL_CategarySwift/Classes/Base/ZLPopOverVC/ZLBasePopContentView.swift
@@ -10,12 +10,12 @@ import UIKit
 
 open class ZLBasePopContentView: UIView {
 
-    override open init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.white
     }
     
-    required open init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
